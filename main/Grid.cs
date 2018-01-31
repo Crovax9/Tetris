4e0b71361de1e1c680d6746b2a6596c28f984832
user150 <crovax9@gmail.com>
2018-01-31 18:22:03 +0900

    Grid
    Grid commit

---
+++ main/Grid.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class Grid
+    {
+        private int gridX;
+        private int gridY;
+
+        private int[,] grid;
+
+        public int[,] gGrid
+        {
+            get { return grid; }
+        }
+
+        public Grid(int _gridX, int _gridY)
+        {
+            gridX = _gridX;
+            gridY = _gridY;
+
+            grid = new int[gridX, gridY];
+        }
+
+
+        public void DrawBoard()
+        {
+            string line = "■";
+            for (int i = 0; i < gridY; ++i)
+            {
+                Console.WriteLine(line);
+            }
+            for (int i = 0; i < gridY; ++i)
+            {
+                Console.SetCursorPosition(18, i);
+                Console.WriteLine(line);
+            }
+            Console.SetCursorPosition(0, 23);
+            for (int i = 0; i < gridX; ++i)
+            {
+
+                Console.Write(line);
+            }
+            Console.WriteLine();
+        }
+    }
+}
